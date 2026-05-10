25a6b5233b1c5514be2a60609fd65be145bb82b8
banothsrikanth <banothsrikanth015@gmail.com>
2026-05-10 17:51:24 +0530

    final changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api/v1"
-      : "https://talk-loop-backend.vercel.app/api/v1",
+      : import.meta.env.VITE_API_URL,
 
   withCredentials: true,
 });
