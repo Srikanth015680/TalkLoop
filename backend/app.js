586d27662d95a050f81a2e27791d6d9368c5458b
banothsrikanth <banothsrikanth015@gmail.com>
2026-05-10 18:45:08 +0530

    cors issue resloved

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://chatapptalkloop.vercel.app",
+     " https://talk-loop-iota.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
